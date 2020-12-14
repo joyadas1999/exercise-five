84d92a2f99eb087276e4df777469e332677f50fe
joyadas1999 <47679860+joyadas1999@users.noreply.github.com>
2020-12-14 03:40:37 -0500

    added user and login and logout

--- src/containers/UserProfile.js
+++ src/containers/UserProfile.js
@@ -1,9 +1,12 @@
 import React from "react";
 
-function UserProfile() {
+import UserProfileComponent from "../components/UserProfileComponent";
+
+function UserProfile({ userInformation }) {
   return (
     <div>
       <h1>User Profile</h1>
+      <UserProfileComponent userInformation={userInformation} />
     </div>
   );
 }
